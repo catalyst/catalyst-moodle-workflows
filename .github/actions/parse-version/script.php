ad99c753b0228eb2e69395fdd3740b2860b6b70c
Kevin Pham <keevan.pham@gmail.com>
2022-04-20 14:57:57 +1000

    feat: add dynamic test matrixes, concurrency checks and more options
    - Dynamic matrixes: workflows will check the version.php file which
      should always exist, and run checks based on the supported versions.
      It will apply a moodle_branches input check, or check the supported
      range, or fallback and check the plugin->requires range. This should
      cover most cases and other edge cases could be fixed by setting the
      appropriate plugin version settings (e.g. requires or supported)
    - Concurrency check: This prevents multiple tests for the same
      combination from running in the workflow. This can happen when
      performing another push to the same PR, this will effectively cancel
      the previous run and should reduce the time taken to run the most
      current workflow.
    - Added master branch to the list of branches to run workflows against
      by default (except when a support range is provided). This can be
      disabled explicitly by providing an input option disable_master
    - Full matrix options is now managed by a single yaml file currently
      located at `.github/actions/matrix/matrix_includes.yml`
    - Automating releases: to test if releases will trigger under the
      appropriate conditions, I've added an additional job to allow us to
      monitor if the release job will be triggered as expected. If so it
      would be safe then, to add the real release job to the workflow, and
      then we can remove the need for having 2 workflows (ci + release) and
      have them run inside a single workflow file.

---
+++ .github/actions/parse-version/script.php
@@ -0,0 +1,130 @@
+<?php
+
+require __DIR__ . '../../../../vendor/autoload.php';
+
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Outputs a value and binds it to a name provided.
+ *
+ * Note that arrays/objects should be JSON encoded, and read via the fromJson
+ * method as described here:
+ * https://docs.github.com/en/actions/learn-github-actions/expressions#fromjson
+ *
+ * @param     string $name name of the output
+ * @param     string $value value of the output - preferably JSON encoded if its an object/array
+ * @author    Kevin Pham <kevinpham@catalyst-au.net>
+ * @copyright Catalyst IT, 2022
+ */
+function output(string $name, string $value) {
+    echo PHP_EOL;
+    echo "::set-output name=$name::$value";
+    echo PHP_EOL;
+    echo "Setting output.. $name = $value";
+    echo PHP_EOL;
+}
+
+// Greet the user.
+echo "Hello from PHP!";
+$workspace = $_SERVER['GITHUB_WORKSPACE'] ?? '';
+
+// This check is only possible with the version.php file, so end early if it doesn't exist.
+$versionFilePath = "$workspace/plugin/version.php";
+if (!file_exists($versionFilePath)) {
+    echo "version.php does not exist";
+    exit(1);
+}
+
+// Some moodle constants to prevent errors when trying to require the version.php file.
+define('MOODLE_INTERNAL', 1);
+/** Software maturity level - internals can be tested using white box techniques. */
+define('MATURITY_ALPHA',    50);
+/** Software maturity level - feature complete, ready for preview and testing. */
+define('MATURITY_BETA',     100);
+/** Software maturity level - tested, will be released unless there are fatal bugs. */
+define('MATURITY_RC',       150);
+/** Software maturity level - ready for production deployment. */
+define('MATURITY_STABLE',   200);
+/** Any version - special value that can be used in $plugin->dependencies in version.php files. */
+define('ANY_VERSION', 'any');
+
+$plugin = new \stdClass();
+
+require_once($versionFilePath);
+
+
+// All supported matrix includes:
+$matrixYaml = file_get_contents("$workspace/ci/.github/actions/matrix/matrix_includes.yml");
+$matrix = Yaml::parse($matrixYaml);
+
+// Version breakpoints are sourced from:
+// https://download.moodle.org/api/1.3/updates.php?format=json&version=0.0&branch=$lowestSupportedBranch
+$updates = json_decode(file_get_contents('https://download.moodle.org/api/1.3/updates.php?format=json&version=0.0&branch=3.3'), true);
+$updates = $updates['updates']['core'] ?? [];
+
+$preparedMatrix = array_filter($matrix['include'], function($entry) use($plugin, $updates) {
+    // Regex and replacement templates - Partially generated from https://regex101.com/
+    $re = '/MOODLE_(.*)_STABLE/m';
+    $subst = '$1';
+    $coreVersion = preg_replace($re, $subst, $entry['moodle-branch']);
+    $disable_master = !empty($_SERVER['INPUT_DISABLE_MASTER']);
+
+    // If a fixed support range is set, use this.
+    if (!empty($plugin->supported)) {
+        [$lower, $upper] = $plugin->supported;
+        if ($lower <= $coreVersion && $coreVersion <= $upper) {
+            return true;
+        }
+        // For non master branches, immediately return false as they should not be included in the run.
+        if ($entry['moodle-branch'] !== 'master') {
+            return false;
+        }
+    }
+
+    if (isset($_SERVER['INPUT_FILTER'])) {
+        $filter = $_SERVER['INPUT_FILTER'];
+        $filter = preg_split('/\s+/', $filter);
+        // Otherwise if not defined, it should check if the INPUT_FILTER variable has been provided, and use the matching versions there instead.
+        if (in_array($entry['moodle-branch'], $filter)) {
+            return true;
+        }
+    }
+
+    // If that hasn't been provided either, it should fallback and do a
+    // requirement check on the version itself. Noting that this is probably the
+    // worst option as it may need to go all the way down to version 3.3 to
+    // support Totara and is probably wrong, as it also assumes the range is
+    // open e.g. 35+
+    if (!empty($plugin->requires)) {
+        // Notes:
+        // - $plugin->requires = lowest version supported
+        // - Use $plugin->requires to ensure the version checked (matching based
+        // on regex) is higher than the lowest supported, if so, include it in
+        // the matrix.
+        foreach ($updates as $apiVersion) {
+            $branchValue = str_replace('.', '', $apiVersion['branch']);
+            $branch = "MOODLE_{$branchValue}_STABLE";
+            if ($entry['moodle-branch'] === $branch && $plugin->requires <= $apiVersion['version']) {
+                return true;
+            }
+        }
+    }
+
+    // Determine whether or not to include the master/dev branch
+    if ($entry['moodle-branch'] === 'master' && !$disable_master) {
+        return true;
+    }
+
+    return false;
+});
+
+$jsonMatrix = json_encode(['include' => array_values($preparedMatrix)], JSON_UNESCAPED_SLASHES);
+output('matrix', $jsonMatrix);
+
+// Output the component / plugin name (which would be useful e.g. for a release)
+output('component', $plugin->component);
+
+// Output the highest available moodle branch in this set, which will be used to
+// determine whether or not various tests/tasks will run, such as grunt.
+output('highest_moodle_branch', end($preparedMatrix)['moodle-branch'] ?? '');
+
